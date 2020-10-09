815791961e7db4efa67d2933bc7bfabb9ec06dbf
David Gonçalves <davidsg.dev@gmail.com>
2020-10-09 19:24:46 -0300

    lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
+import { Container } from 'react-grid-system';
 
-import Button from './components/Button/Button';
 import Home from './pages/home/Home';
-import { Container  } from 'react-grid-system';
 
 const App = () => (
-  <Container  >
-    {/*<Button sizeVertical="S" sizeHorizontal="container" colorTheme="default" type="submit">Seguir</Button>*/}
-
-    {/*<Button sizeVertical="S" sizeHorizontal="text" colorTheme="default" type="submit">Seguir</Button>*/}
-    {/*<Button sizeVertical="M" sizeHorizontal="text" colorTheme="default" type="submit">Seguir</Button>*/}
-    {/*<Button sizeVertical="L" sizeHorizontal="text" colorTheme="default" type="submit">Seguir</Button>*/}
-
-    {/*<Button sizeVertical="S" sizeHorizontal="text" colorTheme="dark" type="submit">Seguir</Button>*/}
-
-    {/*<Button sizeVertical="S" sizeHorizontal="container" colorTheme="dark" type="submit">Seguir</Button>*/}
+  <Container>
     <Home />
   </Container>
 );
